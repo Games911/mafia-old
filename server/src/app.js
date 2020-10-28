44a98b596f567d9799357fc165e1406bc5a427f0
kris-sure <kukulyak.taras@gmail.com>
2020-10-28 22:10:32 +0200

    Finish connect to mongo

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const config = require('./config/init');
 const loginController = require('./api/controllers/loginController');
 const authRoutes = require('./api/routes/auth');
 const userRoutes = require('./api/routes/user');
@@ -9,11 +8,8 @@ const userRoutes = require('./api/routes/user');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(config.cors);
-
 app.get('/', async(req, res, next) => {
     const result = await loginController.login(req.body.name);
-    console.log(result);
     res.status(200).json({
         message: result
     });
