7f683809b0bdeac971c39d483be2517ae0a9617b
kris-sure <kukulyak.taras@gmail.com>
2020-11-29 15:53:52 +0200

    Create rooms endpoints for server

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const homeController = require('./api/controllers/homeController');
 const authRoutes = require('./api/routes/auth/auth');
 const seedRoutes = require('./api/routes/system/seed');
+const roomRoutes = require('./api/routes/room/room');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -23,5 +24,6 @@ app.get('/', async(req, res) => {
 
 app.use('/auth', authRoutes);
 app.use('/seed', seedRoutes);
+app.use('/room', roomRoutes);
 
 module.exports = app;
---
+++ server/src/database/seeds/seedRoom.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const {hashPassword} = require('../../api/services/auth/passwordService');
+const Room = require('../models/room/Room');
+const User = require('../models/auth/User');
+
+const seedRoom = async () => {
+
+    Room.remove({}, function() {
+
+    });
+
+    const passwordHash = await hashPassword("123456");
+
+    const user4 = new User({
+        _id: new mongoose.Types.ObjectId(),
+        email: "user4@gmail.com",
+        nikname: "user4",
+        password: passwordHash,
+    });
+
+    const user5 = new User({
+        _id: new mongoose.Types.ObjectId(),
+        email: "user5@gmail.com",
+        nikname: "user5",
+        password: passwordHash,
+    });
+
+    const user6 = new User({
+        _id: new mongoose.Types.ObjectId(),
+        email: "user6@gmail.com",
+        nikname: "user6",
+        password: passwordHash,
+    });
+
+    const user7 = new User({
+        _id: new mongoose.Types.ObjectId(),
+        email: "user7@gmail.com",
+        nikname: "user7",
+        password: passwordHash,
+    });
+    const documentsUser = [user4, user5, user6, user7];
+    await User.create(documentsUser);
+
+
+    const room1 = new Room({
+        _id: new mongoose.Types.ObjectId(),
+        name: 'Room1',
+        status: 'free',
+        players: [user4, user5],
+        createdBy: [user4],
+    });
+
+    const room2 = new Room({
+        _id: new mongoose.Types.ObjectId(),
+        name: 'Room2',
+        status: 'free',
+        players: [user6, user7],
+        createdBy: [user6],
+    });
+
+    const documentsRoom = [room1, room2];
+    await Room.create(documentsRoom);
+
+};
+
+module.exports = seedRoom;
